d33499bca3f662cf6f1e2e2125e5c1478968bb49
Leon Gurin <leon9gur@gmail.com>
2023-04-02 16:39:08 +0300

    Initialized Repo :+1:

---
+++ HW1/readTrainData.py
@@ -0,0 +1,18 @@
+
+def readTrainData(file_name):
+    file = open(file_name, 'r')
+    lines = file.readlines()
+    texAll = []
+    lbAll = []
+    voc = []
+    for line in lines:
+        splitted = line.split('\t')
+        lbAll.append(splitted[0])
+        texAll.append(splitted[1].split())
+        words = splitted[1].split()
+        for w in words:
+            voc.append(w)
+    voc = set(voc)
+    cat = set(lbAll)
+    return texAll, lbAll, voc, cat
+
