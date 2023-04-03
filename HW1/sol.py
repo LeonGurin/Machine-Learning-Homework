ec4db35841fbf13fd1730818307881a395e90bf7
Leon Gurin <leon9gur@gmail.com>
2023-04-04 00:56:58 +0300

    Finished question 1 I think :tea:

--- HW1/sol.py
+++ HW1/sol.py
@@ -1,14 +1,17 @@
-import readTrainData
+import readTrainData as rdt
 
+def learn_NB_text():
+    return
 
-
+def ClassifyNB_text(Pw, P):
+    return
 
 def main():
-    texAll, lbAll, voc, cat = readTrainData("train.txt")
+    texAll, lbAll, voc, cat = rdt.readTrainData("r8-train-stemmed.txt")
+
     Pw, P = learn_NB_text()
-    
-
-
+    suc = ClassifyNB_text(Pw, P)
+    print(suc)
 
 if __name__ == "__main__":
     main()
