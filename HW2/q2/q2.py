e160a04c7a3aec9011254e0a9d9e5a6c414db234
Leon Gurin <leon9gur@gmail.com>
2023-06-14 18:35:41 +0300

    more human pca 🚣‍♂️

--- HW2/q2/q2.py
+++ HW2/q2/q2.py
@@ -1,62 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 from scipy.spatial import distance_matrix
-from numpy.linalg import eig
 from sklearn.metrics import accuracy_score
 import os
-
-class PCA:
-    def __init__(self, n_components):
-        self.n_components = n_components
-        self.components = None
-        self.mean = None
-        self.explained_variance = None
-
-    def fit(self, X):
-        self.mean = np.mean(X, axis=0)
-        X = X - self.mean
-
-        cov_matrix = np.cov(X.T)
-        eigenvalues, eigenvectors = eig(cov_matrix)
-
-        self.components = eigenvectors.T[:self.n_components]
-        self.explained_variance = eigenvalues[:self.n_components]
-
-    def transform(self, X):
-        X = X - self.mean
-        return np.dot(X, self.components.T)
-
-    def inverse_transform(self, X):
-        return np.dot(X, self.components) + self.mean
+import timeit as time
 
 def plot_cdf(data):
-    sorted_data = np.sort(data)[::-1]
+	sorted_data = np.sort(data)[::-1]
 
-    data_cumsum = np.cumsum(sorted_data)
-    data_normalized = data_cumsum / data_cumsum[-1]
+	data_cumsum = np.cumsum(sorted_data)
+	data_normalized = data_cumsum / data_cumsum[-1]
 
-    plt.plot(np.arange(1, len(sorted_data)+1), data_normalized)
-    plt.xlabel('Principal Component')
-    plt.ylabel('Cumulative Proportion of Variance')
-    plt.title('Cumulative Distribution Function of Eigenvalues')
-    plt.show()
-
-def plot_images(original, reduced, recovered):
-    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-
-    axes[0].imshow(original.reshape(28, 28), cmap='gray')
-    axes[0].set_title('Original Image')
-    axes[0].axis('off')
-
-    axes[1].imshow(reduced.reshape(9, 9), cmap='gray')
-    axes[1].set_title('Image after PCA')
-    axes[1].axis('off')
-
-    axes[2].imshow(recovered.reshape(28, 28), cmap='gray')
-    axes[2].set_title('Recovered Image')
-    axes[2].axis('off')
-
-    plt.show()
+	# Plot the CDF of eigenvalues
+	plt.plot(np.arange(1, len(sorted_data)+1), data_normalized)
+	plt.xlabel('Principal Component')
+	plt.ylabel('Cumulative Proportion of Variance')
+	plt.title('Cumulative Distribution Function of Eigenvalues')
+	plt.show()
 
 def load_data(train_file, test_file):
     # Load data from files.
@@ -72,7 +33,53 @@ def load_data(train_file, test_file):
 
     return train_X, train_y, test_X, test_y
 
-def knn(train_X, train_y, test_X, test_y, k, batch_size=100):
+def performPCA(data, k=9, plot=False):
+	mu = np.mean(data, axis=0)
+
+	# subtract the mean from the data
+	Z = data - mu
+
+	S = np.matmul(Z.T, Z)
+	eigenvalues, eigenvectors = np.linalg.eig(S)
+
+	# sort the eigenvalues and eigenvectors in descending order
+	idx = np.argsort(eigenvalues)[::-1]
+	eigenvalues = eigenvalues[idx]
+	eigenvectors = eigenvectors[:, idx]
+
+	# plot the CDF of eigenvalues
+	if plot:
+		plot_cdf(eigenvalues)
+
+	# select the top k^2 eigenvectors
+	E = eigenvectors[:, :(k ** 2)].T
+	y = np.matmul(E, Z.T)
+
+	# pick a random image and reconstruct it
+	idx = np.random.randint(0, data.shape[0])
+	original = data[idx]
+	PCA_image = np.matmul(E, (original - mu))
+	reconstructed = np.matmul(E.T, PCA_image) + mu
+
+	original = original.reshape(28, 28)
+	PCA_image = PCA_image.reshape(k, k)
+	reconstructed = reconstructed.reshape(28, 28)
+	
+	# plot the original and reconstructed images
+	if plot:
+		fig, ax = plt.subplots(1, 3)
+		ax[0].imshow(original, cmap='gray')
+		ax[0].set_title('Original Image')
+		ax[1].imshow(PCA_image, cmap='gray')
+		ax[1].set_title('PCA Image')
+		ax[2].imshow(reconstructed, cmap='gray')
+		ax[2].set_title('Reconstructed Image')
+		plt.show()
+
+	return y.T, E
+
+def performKNN(train_X, train_y, test_X, test_y, k, batch_size=100):
+    # Separate the data into batches to avoid memory issues
     num_test_samples = test_X.shape[0]
     num_batches = int(np.ceil(num_test_samples / batch_size))
 
@@ -81,42 +88,52 @@ def knn(train_X, train_y, test_X, test_y, k, batch_size=100):
     for i in range(num_batches):
         print(f"Batch {i+1}/{num_batches}")
         start_idx = i * batch_size
-        end_idx = min((i + 1) * batch_size, num_test_samples)
+        end_idx = np.min((i + 1) * batch_size, num_test_samples)
 
         test_X_batch = test_X[start_idx:end_idx]
 
+		# compute the distance matrix between the test and train data
         dist_matrix = distance_matrix(test_X_batch, train_X)
+	
+		# find the k nearest neighbors
         neighbors_indices = np.argpartition(dist_matrix, k, axis=1)[:, :k]
 
-        neighbors_labels = train_y[neighbors_indices].astype(int)
+		# find the most common label among the k nearest neighbors
+        neighbors_labels = train_y[neighbors_indices]
+	
+		# assign the most common label to the test sample
         test_preds[start_idx:end_idx] = np.apply_along_axis(lambda x: np.bincount(x).argmax(), axis=1, arr=neighbors_labels)
 
     return test_preds
 
-train_X, train_y, test_X, test_y = load_data("fashion-mnist_train.csv", "fashion-mnist_test.csv")
+if __name__ == '__main__':
+	# start timer	
+	start = time.default_timer()
 
-pca = PCA(n_components=81)
-pca.fit(train_X)
-train_X_reduced = pca.transform(train_X)
-test_X_reduced = pca.transform(test_X)
+	# read the data
+	train_data, train_labels, test_data, test_labels = load_data('fashion-mnist_train.csv', 'fashion-mnist_test.csv')
 
-plot_images(train_X[0], train_X_reduced[0], pca.inverse_transform(train_X_reduced[0]))
+	# use PCA to reduce dimensionality from 28 by 28 to 9 by 9
+	PCA_train_data = performPCA(train_data, plot=True)
+	
+	# test the model on the test data
+	optimal_dim_PCA = 10
+	PCA_train_data, E = performPCA(train_data, k=optimal_dim_PCA)
+	PCA_test_data = np.matmul(E, (test_data - np.mean(test_data, axis=0)).T).T
 
-plt.show()
+	print(PCA_train_data.shape, PCA_test_data.shape)
 
-plot_cdf(pca.explained_variance)
+	# commented out because it takes a long time to run 😭	
+	# k_values = range(1, 15)
+	# accuracies = [performKNN(PCA_train_data, train_class_col, PCA_test_data, test_class_col, k) for k in k_values]
+	# optimal_k = k_values[np.argmax(accuracies)]
 
-optimal_dim = 100
-pca_optimal = PCA(n_components=optimal_dim)
-pca_optimal.fit(train_X)
-train_X_optimal = pca_optimal.transform(train_X)
-test_X_optimal = pca_optimal.transform(test_X)
+	optimal_k_KNN = 10
+	test_preds = performKNN(PCA_train_data, train_labels, PCA_test_data, test_labels, k=optimal_k_KNN, batch_size=100)
+	accuracy = accuracy_score(test_labels, test_preds)
+	print(f"Test accuracy is: {accuracy * 100}%")
 
-# k_values = range(1, 15)
-# k_values = [10]
-# accuracies = [knn(train_X_optimal, train_y, test_X_optimal, test_y, k) for k in k_values]
-# optimal_k = k_values[np.argmax(accuracies)]
+	# stop timer
+	stop = time.default_timer()
+	print('Time: ', stop - start)
 
-test_preds = knn(train_X_optimal, train_y, test_X_optimal, test_y, 10)
-accuracy = accuracy_score(test_y, test_preds)
-print(f"Test accuracy is: {accuracy * 100}%")
